a1cb1aae6008c459cf6a4ad953b602a18208f836
emirdeval <emir.alievqw@gmail.com>
2023-12-07 14:29:51 +0400

    test

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ const renameJS = ({ dirname, basename, extname }) => ({
 gulp.task("deploy", function () {
   return gulp.src("./dist/**/*").pipe(
     deploy({
-      remoteUrl: "https://github.com/EmirAliyev/justlanding.git",
+      remoteUrl: "https://github.com/EmirAliyev/responsive.git",
       branch: "main",
     })
   );
